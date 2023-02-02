35faddd6694f1688ff35344118057baa5190fe5d
giffarine1501 <giffarine1501@gmail.com>
2023-02-02 17:56:04 +0700

    update020266

---
+++ footer_gift.php
@@ -0,0 +1,48 @@
+<footer class=" my-md-5 pt-md-5 border-top bg-blue">
+    <div class="row mr-0 ml-0">
+		<div class="col">
+		<div class="col-12 col-md">
+			<small class="d-block mb-3 text-muted"><img src="assets/img/logo-footer.png" class="logo-footer-set"></small>
+		</div>
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			<small class="text-muted pl-4">สำนักงาน ป.ป.ส เลขที่ 5 ถนนดินแดง แขวงสามเสนใน </small> 
+			<small class="text-muted pl-4">เขตพญาไท กรุงเทพมหานคร 10400</small> 
+
+		</div>
+		</div>
+		
+		<div class="col-6 col-md">
+			<h5 class="text-muted"> แผนผังเว็บไซต์ </h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> หน้าแรก </a></li>
+				<li><a class="text-muted" href="#"> เกี่ยวกับเรา </a></li>
+				<li><a class="text-muted" href="#"> ข้อมูล ปปส.อาเซียน </a></li>
+				<li><a class="text-muted" href="#"> โครงการสำคัญ </a></li>
+				
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<h5 class="text-muted">  ฟฟ</h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> รายงานเฝ้าระวังยาเสพติด </a></li>
+				<li><a class="text-muted" href="#"> รายงาน </a></li>
+				<li><a class="text-muted" href="#"> ปฏิทินกิจกรรม </a></li>
+				<li><a class="text-muted" href="#">ติดต่อเรา </a></li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<h5 class="text-muted"> ช่องทางติดต่อเรา </h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> โทรศัพท์ : 02-247-0901-19 </a></li>
+				<li><a class="text-muted" href="#"> โทรสาร : 02-246-8526 </a></li>
+				<li><a class="text-muted" href="#"> อีเมล : aseannarco@oncb.go.th </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+			</ul>
+		</div>
+    </div>
+	
+	<div class="col-12 col-md text-center line">
+		<a class="text-muted ">&copy; สำนักงาน ป.ป.ส. อาเซียน (ASEAN Narcotics Cooperation Center) </a>
+	</div>
+	
+</footer>
---
+++ index_gift.php
@@ -0,0 +1,103 @@
+<!-- -----------start header----------- -->
+<?php include('comtop.php'); ?>
+<?php include('header.php'); ?>
+<!-- -----------stop header----------- -->
+
+<!-- -----------start banner----------- -->
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/banner/banner1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/banner/banner2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/banner/banner3.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+<!-- -----------stop banner----------- -->
+
+<!-- -----------start icon shotcut----------- -->
+<div class="bg-short">
+  <div class="col-12">
+    <div id="Main_banner_slideIndicators" class="carousel slide rounded" data-ride="carousel">
+      <ol class="carousel-indicators" id="slide_serve">
+        <li data-target="#Main_banner_slideIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#Main_banner_slideIndicators" data-slide-to="1" class=""></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s1.png" alt="" class="img-set">
+                <h7 href="#">โครงการสำคัญ</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s2.png" alt="" class="img-set">
+                <h7 href="#">เฝ้าระวังยาเสพติด</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s3.png" alt="" class="img-set">
+                <h7 href="#">งานวิจัย</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s4.png" alt="" class="img-set">
+                <h7 href="#">รายงานการจับกุม</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s5.png" alt="" class="img-set">
+                <h7 href="#">กระดานถามตอบ</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s6.png" alt="" class="img-set">
+                <h7 href="#">โครงการ AAITF</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s7.png" alt="" class="img-set">
+                <h7 href="#">ADMN SYSTEM</h7>
+              </a>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6 col-6 mt-3 mb-3 text-center">
+              <a class="box-set" href="#">
+                <img src="assets/img/icon/s8.png" alt="" class="img-set">
+                <h7 href="#">เว็บลิงค์</h7>
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<!-- -----------start footer----------- -->
+<?php include('footer.php'); ?>
+<?php include('combottom.php'); ?>
+<!-- -----------stop footer----------- -->
