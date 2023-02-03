57bdc00198d3527d2bdf8085fc8847dcee4251d9
giffarine1501 <giffarine1501@gmail.com>
2023-02-03 18:04:53 +0700

    update 03/02/66 new

--- banner.php
+++ banner.php
@@ -1,4 +1,4 @@
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carousel_banner_slide" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="assets/img/banner/banner1.png" class="d-block w-100" alt="...">
@@ -10,11 +10,11 @@
       <img src="assets/img/banner/banner3.png" class="d-block w-100" alt="...">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+  <button class="carousel-control-prev" type="button" data-target="#carousel_banner_slide" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+  <button class="carousel-control-next" type="button" data-target="#carousel_banner_slide" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </button>
